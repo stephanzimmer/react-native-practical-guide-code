234f601a9751104f05f9771e4579c6dbf381e1fc
stephan <guroo@gmx.de>
2025-03-25 22:23:34 +0100

    button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
+  function handlePress() {
+    console.error('test')
+    alert('test')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View pointerEvents="box-none"  style={styles.container}>
+      <Button title="Press me3" onPress={handlePress} />
     </View>
   );
 }
@@ -13,8 +17,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aac',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
+
+// import React from 'react';
+// import {Text, View, Pressable} from 'react-native';
+// //import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
+// export default function App() {
+//     return (
+//         <View style={{padding: 50}}>
+//             <Pressable onPress={() => console.log("press")}>
+//             <View
+//                 style={{
+//                     backgroundColor: 'red',
+//                     width: 100,
+//                     height: 100,
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                 }}>
+//                 <Text>1</Text>
+//             </View>
+//             </Pressable>
+//             <View
+//                 style={{
+//                     backgroundColor: 'blue',
+//                     width: 100,
+//                     height: 100,
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                 }}>
+//                 <Text>2</Text>
+//             </View>
+//             <View
+//                 style={{
+//                     backgroundColor: 'green',
+//                     width: 100,
+//                     height: 100,
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                 }}>
+//                 <Text>3</Text>
+//             </View>
+//         </View>
+//     );
+// }
